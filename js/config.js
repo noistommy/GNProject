498f5bac4c2c7a8e0b714a1d3c2eb9a13e309283
noistommy <mykim@n3n.co.kr>
2018-11-05 09:53:52 +0900

    GN Project Front End

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var BASE_ROOT;
+var BASE_ROOT_W;
+var testMode = false;
+
+if (testMode) {
+    BASE_ROOT = "http://172.16.10.51:21002/top";
+    BASE_ROOT_W = "http://172.16.10.51:21002";
+} else {
+    BASE_ROOT = "/top";
+    BASE_ROOT_W = "";
+}
